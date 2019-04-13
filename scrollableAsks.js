e460c198d1a06a120e2f7521113958d5e67cccec
Jim Pick <jim@backblend.com>
2019-04-12 21:45:16 -0700

    Add keystroke handling

--- scrollableAsks.js
+++ scrollableAsks.js
@@ -1,15 +1,75 @@
 import React, { useState, useEffect } from 'react'
-import { Box, Text } from 'ink'
+import { Box, Text, StdinContext } from 'ink'
+import figures from 'figures'
 
-export default function ScrollableAsks ({ height }) {
+const globalState = {
+  lastKey: null
+}
+
+function ScrollableAsks ({ height, stdin, setRawMode }) {
+  const [updateTime, setUpdateTime] = useState()
+  const [cursorIndex, setCursorIndex] = useState(0)
+
+  const state = { cursorIndex }
+
+  useEffect(() => {
+    setRawMode(true)
+    stdin.on('data', handler)
+    return () => stdin.removeListener('data', handler)
+
+    function handler (data) {
+      if (data === '\u001b[A') {
+        data = 'up'
+        if (state.cursorIndex > 0) {
+          setCursorIndex(state.cursorIndex - 1)
+        }
+      }
+      if (data === '\u001b[B') {
+        data = 'down ' + state.cursorIndex
+        if (state.cursorIndex < height - 1) {
+          setCursorIndex(state.cursorIndex + 1)
+        }
+      }
+      if (data === '\u001b[D') {
+        data = 'left'
+      }
+      if (data === '\u001b[C') {
+        data = 'right'
+      }
+      if (data === '\r') {
+        data = 'return'
+      }
+      globalState.lastKey = data
+      setUpdateTime(Date.now())
+    }
+  }, [cursorIndex])
+
   const rows = []
   for (let i = 0; i < height; i++) {
-    rows.push(<Box>Row {i + 1} of {height}</Box>)
+    const pointer = (i === cursorIndex) ? figures.pointer : ' ' 
+    rows.push(
+      <Box>{pointer} Row {i + 1} of {height}{' '}
+      LastKey {JSON.stringify(globalState.lastKey)}
+      </Box>
+    )
   }
   return (
     <Box flexDirection="column">
       {rows}
     </Box>
+  )
+}
+
+export default function ScrollableAsksWithStdin ({ height }) {
+  return (
+    <StdinContext.Consumer>
+      {({stdin, setRawMode}) => (
+        <ScrollableAsks
+          height={height}
+          stdin={stdin}
+          setRawMode={setRawMode} />
+      )}
+    </StdinContext.Consumer>
   )
 }
 
