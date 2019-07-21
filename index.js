96f86c87f5515288069187be5ca64786dde66d84
Jim Pick <jim@backblend.com>
2019-07-20 23:51:23 -0700

    Update deps with error handling

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ const cli = meow(
 const args = cli.flags
 
 const Main = () => {
-  const [nickname] = useFilecoinConfig('heartbeat.nickname')
-  const [, height, updateTime] = useFilecoinHead({
+  const [, nickname] = useFilecoinConfig('heartbeat.nickname')
+  const [, , height, updateTime] = useFilecoinHead({
     interval: 5000
   })
-  const [netName, , netHeight] = useFilecoinNetworkInfo({
+  const [, netName, , netHeight] = useFilecoinNetworkInfo({
     interval: 30000
   })
   const [unfilteredAsks] = useFilecoinAsks()
